5d4894f3e09c7df4f4b391a512d0051531f36811
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-06 17:44:02 -0500

    Create constant file to store theme

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme = {
+  colors: {
+    primary: '#2b90d9',
+    classicprimarycolor: '#9baec8',
+    classicSecondaryColor: '#d9e1e8',
+    baseColor: '#282c37',
+    successgreen: '#79bd9a',
+    errorRed: '#df405a',
+    warningRed: '#ff5050',
+    text: {
+      primary: '#fff',
+    },
+    inputs: {
+      bgDark: '#131419',
+    },
+  },
+};
