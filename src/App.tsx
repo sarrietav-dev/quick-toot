46383b1c95dd4976598a0c30bd80effb5ef40991
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-06 17:35:13 -0500

    Use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { ComposeForm } from './pages/ComposeForm.page';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Auth } from './pages/Auth.page';
 
 export default function App(): JSX.Element {
-  return <ComposeForm />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ComposeForm} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Router>
+  );
 }
