a6e1dd95f1789ea5d080607cabe5a777fd61ba60
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-06 15:15:02 -0500

    Use CSS instead of SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
-import './sass/main.scss';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
