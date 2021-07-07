1a6f6001ae01e761cd64ca614a4922bbf1e36fca
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-07-06 21:13:27 -0500

    Add credentials reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import contentWarningSlice from './reducers/content-warning.reducer';
 import { configureStore } from '@reduxjs/toolkit';
 import charCounterReducer from './reducers/char-counter.reducer';
+import credentialsReducer from './reducers/credentials.reducer';
 
 export const storeConfig = {
   reducer: {
     contentWarning: contentWarningSlice,
     charCounter: charCounterReducer,
+    credentials: credentialsReducer,
   },
 };
 
